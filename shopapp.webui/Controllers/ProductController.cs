a3b1b47a2fef3c4ff86e0af858a94429af773c6e
tolgaozkann <ozkanozkan3233@gmail.com>
2022-06-11 18:58:49 +0300

    ASP.NET Project

---
+++ shopapp.webui/Controllers/ProductController.cs
@@ -0,0 +1,46 @@
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.AspNetCore.Mvc;
+using shopapp.webui.Data;
+using shopapp.webui.Models;
+
+namespace shopapp.webui.Controllers
+{
+    public class ProductController:Controller
+    {
+        public IActionResult Index()
+        {
+            var product = new Product {Name="Iphone X",Price=6000,Description="güzel telefon"};
+
+            // ViewData["Category"] = "Telefonlar";
+            // ViewData["Product"] = product;
+
+            ViewBag.Category = "Telefonlar";
+            // ViewBag.Product = product;
+
+            // ViewBag.Product
+            return View(product);
+        }
+        public IActionResult list(int? id) 
+        {
+            var products = ProductRepository.Products;
+
+            if (id != null)
+            {
+                products = products.Where( p => p.CategoryId == id).ToList();
+            }
+
+            var productViewModel = new ProductViewModel()
+            {
+                Products = products
+            };
+
+            return View(productViewModel);
+        }
+
+        public IActionResult Details(int id)
+        {
+            return View(ProductRepository.GetProductById(id));
+        }
+    }
+}
---
+++ shopapp.webui/Data/ProductRepository.cs
@@ -0,0 +1,42 @@
+using System.Collections.Generic;
+using System.Linq;
+using shopapp.webui.Models;
+
+namespace shopapp.webui.Data
+{
+    public static class ProductRepository
+    {
+        private static List<Product> _products = null;
+
+        static ProductRepository(){
+            _products = new List<Product>
+            {
+                new Product {ProductId = 1,Name="Iphone 7",Price=3000,Description="iyi telefon",IsApproved=false,ImageUrl = "1.jpg",CategoryId = 1},
+                new Product {ProductId = 2,Name="Iphone 8",Price=4000,Description="çok iyi telefon",IsApproved=true,ImageUrl = "2.jpg",CategoryId = 1},
+                new Product {ProductId = 3,Name="Iphone X",Price=5000,Description="çok iyi telefon",IsApproved=true,ImageUrl = "3.jpg",CategoryId = 1},
+                new Product {ProductId = 4,Name="Iphone 11",Price=7000,Description="çok iyi telefon",ImageUrl = "4.jpg",CategoryId = 1},
+                new Product {ProductId = 5,Name="Lernovo 7",Price=3000,Description="iyi bilgisayar",IsApproved=false,ImageUrl = "1.jpg",CategoryId = 2},
+                new Product {ProductId = 6,Name="Lernovo 8",Price=4000,Description="çok iyi bilgisayar",IsApproved=true,ImageUrl = "2.jpg",CategoryId = 2},
+                new Product {ProductId = 7,Name="Lernovo X",Price=5000,Description="çok iyi bilgisayar",IsApproved=true,ImageUrl = "3.jpg",CategoryId = 2},
+                new Product {ProductId = 8,Name="Lernovo 11",Price=7000,Description="çok iyi bilgisayar",ImageUrl = "4.jpg",CategoryId = 2},
+            };
+        }
+
+        public static List<Product> Products{
+            get
+            {
+                return _products;
+            }
+        }
+
+        public static void AddProduct(Product product)
+        {
+            _products.Add(product);
+        }
+
+        public static Product GetProductById(int productId)
+        {
+            return _products.FirstOrDefault(p => p.ProductId ==productId);
+        }
+    }
+}
